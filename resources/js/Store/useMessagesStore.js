e76a4bf32723bac8e6fc48568f1eb4818e24f488
Marko <markostevic96>
2024-11-27 17:36:41 +0100

    Creating a Pinia store

---
+++ resources/js/Store/useMessagesStore.js
@@ -0,0 +1,13 @@
+import {defineStore} from "pinia";
+export const useMessagesStore  = defineStore('messages',{
+    state : () => ({
+        page : 1,
+        messages : []
+    }),
+    getters : {
+        allMessages(state){
+            return state.messages
+        }
+    }
+
+})
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
+import {createPinia} from "pinia";
+const pinia = createPinia();
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -19,6 +20,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(pinia)
             .mount(el);
     },
     progress: {
